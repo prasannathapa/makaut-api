27a26704c531759d24b6395c8c51e86dc2b35e87
Prasanna Thapa <prasannathapax7@gmail.com>
2021-04-09 00:45:01 +0530

    added cloud backup

--- exam.js
+++ exam.js
@@ -3,18 +3,22 @@ const JSDom = require('./node_modules/jsdom')
 const main = require('./index');
 const pdfExt = require('./dataExtractor');
 const PDFParser = require("./node_modules/pdf2json");
+const DB = require('./mongoStore')
 
 let cookieJar = request.jar();
 
-module.exports.getCsrfToken = async function (callback) {
-    request.get({ url: 'https://makaut1.ucanapply.com/smartexam/public/result-details', jar: cookieJar }, (error, response, body) => {
-        if(error)
-            console.log('statusCode:', response && response.statusCode, 'error:', error); 
-        let jsDom = new JSDom.JSDOM(body);
-        element = jsDom.window.document.querySelectorAll("meta[name='csrf-token'")[0];
-        return element ? callback(element.getAttribute("content")) : callback(undefined);
-    });
+module.exports.getCsrfToken = async function () {
+    return new Promise((resolve,reject)=>{
+        request.get({ url: 'https://makaut1.ucanapply.com/smartexam/public/result-details', jar: cookieJar }, (error, response, body) => {
+            if(error)
+                console.log('statusCode:', response && response.statusCode, 'error:', error); 
+            let jsDom = new JSDom.JSDOM(body);
+            element = jsDom.window.document.querySelectorAll("meta[name='csrf-token'")[0];
+            return element ? resolve(element.getAttribute("content")) : resolve(null);
+        });
+    })
 };
+
 module.exports.getMarkSheetPDF = async function (csrf, sem, roll, callback) {
 
     let formData = {
@@ -31,13 +35,15 @@ module.exports.getMarkSheetPDF = async function (csrf, sem, roll, callback) {
             let pdfParser = new PDFParser();
             pdfParser.parseBuffer(body);
             pdfParser.on("pdfParser_dataError", errData => {callback({info:"Records not found", error:errData.parserError}); main.resetCSRF();});
-            pdfParser.on("pdfParser_dataReady", pdfData => callback(pdfExt.getTextArray(pdfData.formImage.Pages[0].Texts)));
+            pdfParser.on("pdfParser_dataReady", pdfData => callback(pdfExt.getTextArray(pdfData.formImage.Pages[0].Texts, sem)));
         }
         else {
+            console.log("FOXERROR",response.statusCodeatus, body);
             callback({info: "Our server boy was caught smuggling marksheet by the professors, please try again",error:"CSRF-MISMATCH"});
-            main.resetCSRF();
         }
-        if(error)
-            console.log(error,"ERROR");
+        if(error){
+            console.log("FOXERROR",error);
+            callback({info: "Error",error:"UNKNOWN"});
+        }
     });
 };
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('./node_modules/express')
 const check = require('./validator');
 const exam = require('./exam');
-
-let csrfToken;
+const DB = require('./mongoStore');
+let csrfToken = {id:null,count:0}
 const port = process.env.PORT || 8080;
 const app = express();
 app.get('/', function (req, res) {
@@ -21,7 +21,7 @@ app.get('/:roll/:sem', function (req, res) {
     console.log("roll", roll)
     console.log("sem", sem)
 
-    sendResponse(sem, roll, responseObject => {
+    sendResponse(sem, roll, {}, responseObject => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(responseObject, null, 3));
     });
@@ -35,7 +35,7 @@ app.get('/:rollbeg/:rollend/:sem', function (req, res) {
     rollEnd = parseInt(rollEnd);
     if (rollBeg > rollEnd)
         [rollBeg, rollEnd] = [rollEnd, rollBeg]
-    if (!check.isRoll(rollBeg) || !check.isRoll(rollEnd) || (rollEnd - rollBeg) > 120 || !check.isSemSingle(sem)) {
+    if (!check.isRoll(rollBeg) || !check.isRoll(rollEnd) || (rollEnd - rollBeg) > 120 || !check.isSem(sem)) {
         res.end();
         return;
     }
@@ -50,13 +50,19 @@ app.get('/:rollbeg/:rollend/:sem', function (req, res) {
     });
 });
 async function sendRangeResponse(sem, rollBeg, rollEnd, callback) {
-    let responseObject = {};
+    let responseObject = {},backUpObj = {};
     let callBackCount = 0; //variable are syncronous
+    await DB.fetchRange(rollBeg, rollEnd, data => {
+        for (let itr = 0; itr < data.length; itr++) {
+            const gradeCard = data[itr];
+            backUpObj[gradeCard.roll] = gradeCard;
+        }
+    });
     for (let itr = rollBeg; itr <= rollEnd; itr++) {
-        sendResponse(sem, itr.toString(), result => {
+        const roll_itr = itr.toString();
+        sendResponse(sem, roll_itr, backUpObj, result => {
             callBackCount++;
-            responseObject[itr.toString()] = result
-            console.log(sem, itr, callBackCount);
+            responseObject[roll_itr] = result
             if (callBackCount == (rollEnd - rollBeg + 1)) {
                 const sortedResult = Object.keys(responseObject).sort().reduce((obj, key) => {
                     obj[key] = responseObject[key];
@@ -67,51 +73,60 @@ async function sendRangeResponse(sem, rollBeg, rollEnd, callback) {
         })
     }
 }
-async function sendResponse(semList, roll, callback) {
+async function sendResponse(semList, roll, backUp, callback) {
     let responseObject = {};
     let callBackCount = 0;
     for (let i in semList) {
         const sem = semList[i];
-        await exam.getCsrfToken(token => {
-            csrfToken = token
-            exam.getMarkSheetPDF(csrfToken, sem, roll, async (data) => {
+        if (backUp[roll] && backUp[roll][sem] && !backUp[roll][sem].info) {
+            callBackCount++;
+            responseObject = backUp[roll];
+            if (callBackCount == semList.length) {
+                callback(responseObject);
+            }
+        }
+        else {
+            let token = await exam.getCsrfToken();
+            exam.getMarkSheetPDF(token, sem, roll, (data) => {
                 //await data.forEach(val => res.write(val + ",\n"))
                 callBackCount++;
+                console.log("GETTING DATA FROM MAKAUT SERVER: ", sem, roll, token,callBackCount);
+               
                 if (data.name && !responseObject.name) responseObject.name = data.name;
                 if (data.roll && !responseObject.roll) responseObject.roll = data.roll;
                 if (data.registration && !responseObject.registration) responseObject.registration = data.registration;
                 if (data.collegeName && !responseObject.collegeName) responseObject.collegeName = data.collegeName;
                 if (!data.error)
-                    responseObject[sem] = data.result;
+                    responseObject[sem] = data[sem];
                 else
                     responseObject[sem] = { info: data.info };
-                if (data.error && data.error == "CSRF-MISMATCH")
-                    sendResponse([sem], roll, callback);
+                //if (data.error && data.error == "CSRF-MISMATCH")
+                    //this.reinitCSRF();
+                    //sendResponse([sem], roll, callback);
                 if (callBackCount == semList.length) {
                     callback(responseObject);
                 }
             });
-        });
-
+            
+        }
     }
 }
 app.get('/restart', function (req, res, next) {
     process.exit(1);
 });
 app.get('/reset', function (req, res) {
-    csrfToken = "";
+    csrfToken = {id:null,count:0};
     console.log(csrfToken)
     res.send("Done! " + csrfToken);
 });
 app.listen(port, () => {
     console.log("server started at http://localhost:" + port);
-
 });
 
 module.exports.resetCSRF = () => {
-    csrfToken = null;
+    csrfToken = {id:null,count:0};
 }
-module.exports.reinitCSRF = () => {
-    csrfToken = null;
-    exam.getCsrfToken(token => { csrfToken = token });
+module.exports.reinitCSRF = async function reinitCSRF(){
+    csrfToken = {id:null,count:0};
+    csrfToken.id = await exam.getCsrfToken();
 }
