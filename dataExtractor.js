6d3ce31a27967d25a44569409def73cd4830906c
Prasanna Thapa <prasannathapax7@gmail.com>
2021-04-14 23:24:13 +0530

    bugFix

--- dataExtractor.js
+++ dataExtractor.js
@@ -11,8 +11,10 @@ module.exports.getTextArray = (pdf) => {
     if(textArray.length <= 2){
         return {info:"No Records Found", error:"Result doesnt exist"};
     }
+    console.log(textArray);
     let resObj={};
     for(let i = 0; i < textArray.length; i++){
+        const text = textArray[i].trim();
         if(textArray[i].startsWith("NAME")){
             resObj.name = textArray[++i];
         }
@@ -22,14 +24,14 @@ module.exports.getTextArray = (pdf) => {
         else if(textArray[i].startsWith("REGISTRATION NO")){
             resObj.registration  = textArray[++i];
         }
-        else if(textArray[i] == 'O' ||
-                textArray[i] == 'E' ||
-                textArray[i] == 'A' ||
-                textArray[i] == 'B' ||
-                textArray[i] == 'C' ||
-                textArray[i] == 'D' ||
-                textArray[i] == 'F' ||
-                textArray[i] == 'I'){
+        else if(text == 'O' ||
+                text == 'E' ||
+                text == 'A' ||
+                text == 'B' ||
+                text == 'C' ||
+                text == 'D' ||
+                text == 'F' ||
+                text == 'I'){
             //resObj.roll = textArray[++i];
             let subCode = ""
             if(isNaN(textArray[i-3]) && textArray[i-3] != "Points")
