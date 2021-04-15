ff8980d9d0c17e1190aadf89658fe98e24c724e8
Prasanna Thapa <54329023+prasannathapa@users.noreply.github.com>
2021-04-15 18:55:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -79,8 +79,8 @@ async function sendSingleResponse(roll, sem) {
             logger.log("Initial Req Size:[", sem.length, "] backup data:[", reqSaved, "]\n" +
                 "New Request Size:[", sem.length - reqSaved, "]  effeciency: ", (reqSaved / sem.length) * 100 + "%");
             sendResponse(sem, roll, backUpObj, responseObject => resolve(sorted(responseObject)));
-        }).catch(()=>{
-            reject({info:"something went wrong", error:"idk"})
+        }).catch((err)=>{
+            reject({info:"something went wrong", error:err.toString()})
         });
     });
 
